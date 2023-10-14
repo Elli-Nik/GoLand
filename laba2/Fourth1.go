2761fdc56a5c64d5017f8509223202dd6280fb30
Elli-Nik <lizok_nike@mail.ru>
2023-10-14 13:54:48 +0300

    House code

---
+++ laba2/Fourth1.go
@@ -0,0 +1,56 @@
+/*Описать дом
+1. Величины
+2. Мебель 5 объектов
+3. Бытовая техника 5 объектов
+4. Состав семьи
+5. Одна функция вызова, которой можно увидеть все
+6. выложить в гитхаб
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	house1 := house{
+		ceilingHeight: 260,
+		square:        45,
+		numRooms:      3,
+		furniture:     []string{"Диван", "Комод", "Кровать", "Кресло", "Стол"},
+		tech:          []string{"Стиральная машина", "Холодильник", "Микроволновая печь", "Телевизор"},
+		family:        []string{"Я", "Кошка"},
+	}
+	house1.showHouse()
+}
+
+type house struct {
+	family        []string
+	furniture     []string
+	tech          []string
+	ceilingHeight float64
+	square        float64
+	numRooms      int
+}
+
+func (h house) showHouse() {
+	fmt.Println("Площадь кв. = ", h.square, "кв.м")
+	fmt.Println("Высота потолка = ", h.ceilingHeight, "см")
+	fmt.Println("Количество комнат = ", h.numRooms, "\n")
+
+	fmt.Println("Мебель в доме: \n")
+	for index, furniture := range h.furniture {
+		fmt.Println(index+1, furniture)
+	}
+	fmt.Println("\n")
+
+	fmt.Print("Бытовая техника: \n")
+	for index, tech := range h.tech {
+		fmt.Println(index+1, tech)
+	}
+	fmt.Println("\n")
+
+	fmt.Println("Жильцы: \n")
+	for index, family := range h.family {
+		fmt.Println(index+1, family)
+	}
+}
